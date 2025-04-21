0e6f46475f97e8d69d0ef4b365e38be61a4d14ee
Ypj.Han <yongpenghan@outlook.com>
2025-04-21 14:26:37 -0400

    added

---
+++ datasets_added/README.dataset.txt
@@ -0,0 +1,6 @@
+# added > 2025-04-21 2:17pm
+https://universe.roboflow.com/nothing-pyaaj/added
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train_added.py
@@ -0,0 +1,69 @@
+from ultralytics import YOLO
+import time
+
+def train_yolo():
+    print("This is train!!!!!!!!!!!!!!!!")
+    # 加载 YOLOv8 预训练模型
+    model = YOLO("yolov8s.pt")
+
+    # 训练参数
+    model.train(
+        data="./datasets_added/data.yaml",  
+        device="cuda",  # 使用 GPU
+        project="runs/refine_train",
+        name="refine_train",
+        batch = 64,
+        lr0=0.0006527556335135585,
+        optimizer = "Adam",
+        epochs=5
+        # 尝试调整数据集增强
+        #mosaic=1.0,
+        #imgsz=768,
+        #scale=0.5,
+        
+    )
+def export_val_predictions():
+    print("This is val!!!!!!!!!!!!!!!!!!!!")
+    model = YOLO(r"./runs/refine_trainadded/weights/best.pt")
+
+    # 预测验证集（val）上的所有图像并保存结果
+    results = model.predict(
+        source=r"./datasets_added/valid/images",  # val 图像路径
+        conf=0.25,                # 置信度阈值
+        save=True,                # 保存预测图像
+        save_txt=True,            # 保存预测框标签文件（YOLO格式）
+        save_conf=True,           # 保存每个框的置信度
+        project="runs/val_predict",   # 预测保存路径
+        name="best_val_output",       # 子目录名称
+    )
+    print("✅ 所有验证图像的预测结果已保存！")  
+
+
+def export_test_predictions():
+    print("This is val!!!!!!!!!!!!!!!!!!!!")
+    model = YOLO(r"./runs/refine_train/weights/best.pt")
+
+    # 预测验证集（val）上的所有图像并保存结果
+    results = model.predict(
+        source=r"./datasets_added/test/images",  # val 图像路径
+        conf=0.25,                # 置信度阈值
+        save=True,                # 保存预测图像
+        save_txt=True,            # 保存预测框标签文件（YOLO格式）
+        save_conf=True,           # 保存每个框的置信度
+        project="runs/test_predict",   # 预测保存路径
+        name="best_test_output",       # 子目录名称
+    )
+    print("✅ 所有验证图像的预测结果已保存！")  
+def test_yolo():
+    # 加载训练完成的模型
+    print("This is test!!!!!!!!!!!!!!!!")
+    model = YOLO(r"./runs/refine_trainadded/weights/best.pt")
+    # 在测试集上评估模型
+    metrics = model.val(data="./datasets_added/data.yaml", split="test")
+    
+    print(metrics) 
+if __name__ == "__main__":
+    train_yolo()
+    export_val_predictions()
+    export_test_predictions()
+    test_yolo()
